c8eaedb053233dd65e95e43b063540373c993092
PaTara <p040399@outlook.com>
2024-08-21 14:24:39 +0300

    whip-whep

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { WHIPClient } from './whip-whep-js/whip.js'
 
 window.addEventListener("DOMContentLoaded", async function() {
 
@@ -17,25 +18,42 @@ window.addEventListener("DOMContentLoaded", async function() {
     navigator.mediaDevices.getUserMedia(videoObj).then((stream) => {
         let streamtest;
         video.srcObject = stream;
-        video.onplay = function() {
+        video.onplay = async function() {
             streamtest = canvas.captureStream();
             videostream.srcObject = streamtest;
-            console.log('streamtest check', streamtest); 
-            console.log('streamtest.getVideoTracks()[0]', streamtest?.getVideoTracks()[0]); 
+            console.log('streamtest check', streamtest);
+            console.log('streamtest.getVideoTracks()[0]', streamtest?.getVideoTracks()[0]);
             streamtest.getTracks().forEach((track) => console.log('track', track));
-        };
+            // Create peer connection
+            const pc = new RTCPeerConnection({ bundlePolicy: "max-bundle" });
 
-        
-        
-        // console.log(video.srcObject, videostream.srcObject, streamtest, streamtest.getVideoTracks()[0]); 
-        
+            //Send all tracks
+            for (const track of streamtest.getTracks()) {
+                pc.addTransceiver(track, { direction: 'sendonly' });
+            }
+
+            // Create WHIP client
+            const whip = new WHIPClient();
+            //const url = "https://video.multi-agent.io/api/webrtc?dst=browser";
+            const url = "http://localhost/go2rtc/api/webrtc?dst=browser"; // Replace with your WHIP endpoint
+
+            // Start publishing
+            await whip.publish(pc, url);
+            console.log("Streaming started successfully!");
+
+            };
+
+
+
+        // console.log(video.srcObject, videostream.srcObject, streamtest, streamtest.getVideoTracks()[0]);
+
 
         // video.crossOrigin = "Anonymous";
         // img.crossOrigin = "Anonymous";
 
         requestAnimationFrame(drawcanvas);
 
-        
+
     }).catch((err) => {
         console.error(`An error occurred: ${err}`);
       });
